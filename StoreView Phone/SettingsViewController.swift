a755902ff1138952e992f70b0180bceb41bcbbac
Max Napadiy <maximn1@msn.com>
2015-08-29 14:59:50 -0700

    Initial Table View added

--- StoreView Phone/SettingsViewController.swift
+++ StoreView Phone/SettingsViewController.swift
@@ -2,47 +2,34 @@
 //  SettingsViewController.swift
 //  StoreView Phone
 //
-//  Created by Max Napadiy on 8/28/15.
+//  Created by Max Napadiy on 8/29/15.
 //  Copyright (c) 2015 Maxed Out Programming, Inc. All rights reserved.
 //
 
 import UIKit
 
 class SettingsViewController: UIViewController, UITableViewDataSource {
-    
-    //Main ViewController
+    //Main ViewController Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        //println("Loaded")
-        // Do any additional setup after loading the view.
+        //Initial Load
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //End Main ViewController
-    
-        //Start Table View Data
-            //Table definitions
-            let settingsTable = [
-                ("Server Settings"),
-                ("Test")
-            ]
-            //End Table definitions
-        func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-            return 1
-            //Number of Sections
-        }
-        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return settingsTable.count
-            //Number of Rows in each section
-        }
-        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            var cell = UITableViewCell()
-            let (name) = settingsTable[indexPath.row]
-            cell.textLabel?.text = name
-            return cell
-            //Cells in each section
-        }
-        //End Table View Data
+    //End of ViewController Functions
+    //Start of Table View Functions
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    //End of Table View Functions
 }
